7441408b95ddd1085e92cb3b1106c634d4203749
Antonis Karamitros <ablackjim228@gmail.com>
2017-07-29 19:44:51 +0100

    Improve script, add default elm-make perf results

--- compileWithFlags.sh
+++ compileWithFlags.sh
@@ -2,11 +2,11 @@
 
 ## declare an array variable
 declare -a arr=('-A16M -qb0'
-                '-A32M -qb0' 
-                '-A64M -qb0' 
-                '-A16M -qg' 
-                '-A32M -qg' 
-                '-A64M -qg' 
+                '-A32M -qb0'
+                '-A64M -qb0'
+                '-A16M -qg'
+                '-A32M -qg'
+                '-A64M -qg'
                 '-N1 -qg'
                 '-N12 -qg'
                 '-N16 -qg'
@@ -16,37 +16,37 @@ declare -a arr=('-A16M -qb0'
                 '-qg'
                 )
 
-ELM_MAKE_BINARY=~/_REPOS_/elm/Elm-Platform/0.18/elm-make/dist/dist-sandbox-aff3f82/build/elm-make/elm-make
 
 ELM_PLATFORM_DIR=~/_REPOS_/elm/Elm-Platform
+ELM_CSS_DIR=~/_REPOS_/elm-css
+ELM_MAKE_BINARY=$ELM_PLATFORM_DIR/0.18/elm-make/dist/dist-sandbox-aff3f82/build/elm-make/elm-make
 
 ## now loop through the above array
 for i in "${arr[@]}"
 do
-    ATTEMPT=$(echo $i | sed -e 's/\s/_/g')
+    COMPILE_OPTIONS=$(echo $i | sed -e 's/\s/_/g')
     echo "----------- removing existing binary" ;
-    rm -v $ELM_MAKE_BINARY
+    sudo rm -fv $ELM_MAKE_BINARY
     echo "----------- cd ~/_REPOS_/elm/"
     cd ~/_REPOS_/elm/
-    echo "----------- Attempt : $ATTEMPT" ;
-    find $ELM_PLATFORM_DIR | rg '.cabal$' | xargs -n 1 sudo sed -i "s/-threaded -O2 -W \".+\"/-threaded -O2 -W \"-with-rtsopts=$i\"/" ;
+    echo "----------- Attempt with options : $COMPILE_OPTIONS" ;
+    find $ELM_PLATFORM_DIR | grep '.cabal$' | xargs -n 1 sudo sed -i "s/-threaded -O2 -W \".+\"/-threaded -O2 -W \"-with-rtsopts=$i\"/" ;
     sudo rg '\-threaded -O2 -W ' $ELM_PLATFORM_DIR ;
-    echo "----------- Compile $ATTEMPT" ;
+    echo "----------- Compile $COMPILE_OPTIONS" ;
     docker exec flamboyant_hypatia runhaskell /opt/elm/BuildFromSource.hs 0.18 ;
     echo "----------- copy backup" ;
-    echo $i " --> " $ATTEMPT
-    cp -v $ELM_MAKE_BINARY ./elm-make.$ATTEMPT ;
+    echo $i " --> " $COMPILE_OPTIONS
+    cp -v $ELM_MAKE_BINARY ./elm-make.$COMPILE_OPTIONS ;
     echo "----------- cd ~/_REPOS_/elm-css/"
-    cd ~/_REPOS_/elm-css/
+    cd $ELM_CSS_DIR
     echo "----------- Clean artifacts" ;
-    rm -rf ~/_REPOS_/elm-css/elm-stuff/build-artifacts ;
+    rm -rf $ELM_CSS_DIR/elm-stuff/build-artifacts ;
     echo "----------- perf with one core" ;
-    perf stat sysconfcpus -n 1 $ELM_MAKE_BINARY &> make.$ATTEMPT.onecore.perf ; 
+    perf stat sysconfcpus -n 1 $ELM_MAKE_BINARY &> make.$COMPILE_OPTIONS.onecore.perf ; 
     echo "----------- Clean artifacts again" ;
-    rm -rf ~/_REPOS_/elm-css/elm-stuff/build-artifacts ;
+    rm -rf $ELM_CSS_DIR/elm-stuff/build-artifacts ;
     echo "----------- perf with all cores" ;
-    perf stat $ELM_MAKE_BINARY &> make.$ATTEMPT.multicore.perf ;
+    perf stat $ELM_MAKE_BINARY &> make.$COMPILE_OPTIONS.multicore.perf ;
     echo "done"
 done
 
-# You can access them using echo "${arr[0]}", "${arr[1]}" also
